8635e459c862dc847d637c025e7b183ff1e41e5e
IngeneraMiro <miroslav@nabytky.com>
2020-06-21 18:55:10 +0300

    new

---
+++ examprep/src/test/java/com/ingenera/examprep/ExamprepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingenera.examprep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamprepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
